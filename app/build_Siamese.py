46cb2a230b6c1c7fc1f1c9cc75784f7faa92355a
maximeDelmas <maxime.delmas@hotmail.fr>
2022-02-03 09:45:49 +0100

    tests for genotoul

--- app/build_Siamese.py
+++ app/build_Siamese.py
@@ -17,37 +17,39 @@ else:
     print('No GPU available, using the CPU instead.')
     device = torch.device("cpu")
 
-device = torch.device("cpu")
+# device = torch.device("cpu")
 print(device)
 
-data_path = "data/quora-question-pairs"
-train_file = "train.csv"
-test_pos_file = "test.csv"
-label_file = "sample_submission.csv"
+if False:
 
-# Reading
-train = pd.read_csv(os.path.join(data_path, train_file), index_col = 0)
-test_pos = pd.read_csv(os.path.join(data_path, test_pos_file), index_col = 0)
-y_label = pd.read_csv(os.path.join(data_path, label_file), index_col = 0)
+    data_path = "data/quora-question-pairs"
+    train_file = "train.csv"
+    test_pos_file = "test.csv"
+    label_file = "sample_submission.csv"
 
-# Fix datasets for NaN values in question1 or question2
-train = fix_dataset(train)
-test_pos = fix_dataset(test_pos)
+    # Reading
+    train = pd.read_csv(os.path.join(data_path, train_file), index_col = 0)
+    test_pos = pd.read_csv(os.path.join(data_path, test_pos_file), index_col = 0)
+    y_label = pd.read_csv(os.path.join(data_path, label_file), index_col = 0)
 
-# join test and y_label
-test_pos = test_pos.join(y_label, on = 'test_id', how = 'left')
+    # Fix datasets for NaN values in question1 or question2
+    train = fix_dataset(train)
+    test_pos = fix_dataset(test_pos)
 
-# test set contains only positive, labels; suffle to create negative examples
-test_neg = test_pos.copy()
-test_neg['question1'] = np.random.permutation(test_neg['question1'])
-test_neg['is_duplicate'] = 0
+    # join test and y_label
+    test_pos = test_pos.join(y_label, on = 'test_id', how = 'left')
 
-# Create final test set
-test = pd.concat([test_pos, test_neg], ignore_index = True)
+    # test set contains only positive, labels; suffle to create negative examples
+    test_neg = test_pos.copy()
+    test_neg['question1'] = np.random.permutation(test_neg['question1'])
+    test_neg['is_duplicate'] = 0
 
-# Reset indexes
-train.reset_index(drop = True, inplace = True)
-test.reset_index(drop = True, inplace = True)
+    # Create final test set
+    test = pd.concat([test_pos, test_neg], ignore_index = True)
+
+    # Reset indexes
+    train.reset_index(drop = True, inplace = True)
+    test.reset_index(drop = True, inplace = True)
 
 # tokeniser
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
@@ -61,9 +63,9 @@ CV_dataset = SiameseNetWorkSentenceDataset(data = CV, tokenizer = tokenizer, max
 model_params = dict({'freeze_embedding': True, 'freeze_encoder_layer': 8, 'freeze_cls_pooler': True})
 
 
-Contrastive_m_list = [5, 10, 20]
-eval_threshold_list = [1, 5, 10]
-batch = 16
+Contrastive_m_list = [10, 20]
+eval_threshold_list = [5, 10]
+batch = 128
 
 for m, th in zip(Contrastive_m_list, eval_threshold_list):
     loss = ConstrastiveLoss(m = m)
@@ -71,4 +73,4 @@ for m, th in zip(Contrastive_m_list, eval_threshold_list):
     print("Testing m = " + str(m) + " and th = " + str(th))
     out_dir = os.path.join("data/SiameseBERT", "batch" + str(batch) + "_m" + str(m) + "_th" + str(th))
     print("Output directory: " + out_dir)
-    cross_validation(model_params = model_params, dataset = CV_dataset, k = 5, loss_fn = loss, eval_threshold = eval_threshold, device = device, out_dir = out_dir, batch_size = batch, nepochs = 1, save = True, random_state = 1)
+    cross_validation(model_params = model_params, dataset = CV_dataset, k = 5, loss_fn = loss, eval_threshold = eval_threshold, device = device, out_dir = out_dir, batch_size = batch, nepochs = 10, save = True, random_state = 1)
